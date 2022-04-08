f65668fd9a9704dec58cb68e72c141f7dddd06b2
Hung Dinh <hung.dnc@gmail.com>
2022-04-08 10:35:21 +0700

    Update action file and add key

--- TiemQuangCao/FrmMain.cs
+++ TiemQuangCao/FrmMain.cs
@@ -1,3 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace TiemQuangCao
 {
     public partial class FrmMain : Form
@@ -6,5 +16,6 @@ namespace TiemQuangCao
         {
             InitializeComponent();
         }
+
     }
 }
--- TiemQuangCao/Program.cs
+++ TiemQuangCao/Program.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace TiemQuangCao
 {
     internal static class Program
@@ -8,9 +14,9 @@ namespace TiemQuangCao
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
         }
     }
