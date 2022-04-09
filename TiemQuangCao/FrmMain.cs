b4e1c811dc9e643d46441ef4a74c5cc1f4e3e350
Hung Dinh <hung.dnc@gmail.com>
2022-04-09 14:57:40 +0700

    Update project with AutoUpdate.NET and configure build action

--- TiemQuangCao/FrmMain.cs
+++ TiemQuangCao/FrmMain.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Windows.Forms;
+using AutoUpdaterDotNET;
 
 namespace TiemQuangCao
 {
@@ -7,7 +11,59 @@ namespace TiemQuangCao
         public FrmMain()
         {
             InitializeComponent();
+
+            bool autoUpdate = true;
+            if (autoUpdate)
+            {
+                this.tryGetUpdateXml();
+            }
         }
 
+        private async void tryGetUpdateXml()
+        {
+            string gitHubLatestReleaseUrl = "https://github.com/nhdinh/tiem-quang-cao/releases/latest";
+            var handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            string redirectedUrl = null;
+
+            using (HttpClient client = new HttpClient(handler))
+            using (HttpResponseMessage response = await client.GetAsync(gitHubLatestReleaseUrl))
+            using (HttpContent content = response.Content)
+            {
+                // ... Read the response to see if we have the redirected url
+                if (response.StatusCode == System.Net.HttpStatusCode.Found)
+                {
+                    HttpResponseHeaders headers = response.Headers;
+                    if (headers != null && headers.Location != null)
+                    {
+                        redirectedUrl = headers.Location.AbsoluteUri;
+                    }
+                }
+            }
+
+            if (redirectedUrl != null)
+            {
+                try
+                {
+                    redirectedUrl += "/update.xml";
+                    using (HttpClient client = new HttpClient(handler))
+                    using (HttpResponseMessage response = await client.GetAsync(redirectedUrl))
+                    using (HttpContent content = response.Content)
+                    {
+                        // ... Read the response
+                        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            AutoUpdater.Start(redirectedUrl);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // do nothing
+                }
+            }
+        }
     }
 }
